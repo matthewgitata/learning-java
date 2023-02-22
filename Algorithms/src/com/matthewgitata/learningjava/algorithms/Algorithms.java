1f91c937e6107c78f034ad877532595b499f1e85
Matthew Gitata <matthewgitata@gmail.com>
2023-02-22 23:54:00 +0300

    Strings:Parse & Search.

--- Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
+++ Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
@@ -1,15 +1,23 @@
 package com.matthewgitata.learningjava.algorithms;
 
-import java.util.Locale;
-
 public class Algorithms {
     public static void main(String[] args) {
-        System.out.println(normalizeString("     Hello There, BUDDY "));
+        String s = "HeLLo";
+        System.out.println(s.contains("LL"));
+        parseContents(s);
     }
 
-    public static String normalizeString(String s) {
-        return s.toLowerCase(Locale.ROOT).
-                trim()
-                .replace(",", "");
+    public static void parseContents(String s) {
+        System.out.println("Option 1");
+        for (char c : s.toCharArray()) {
+            System.out.print(c);
+        }
+
+        System.out.println();
+        System.out.println("Option 2");
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            System.out.print(c);
+        }
     }
 }
