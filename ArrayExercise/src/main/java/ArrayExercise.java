2178f96312a8ffa72785f6096a25eb7eb59daed5
Matthew Gitata <matthewgitata@gmail.com>
2023-02-22 04:30:00 +0300

    Array Exercise - Uppercase & Increment.

---
+++ ArrayExercise/src/main/java/ArrayExercise.java
@@ -0,0 +1,15 @@
+package main.java;
+
+public class ArrayExercise {
+    public void arrayToUppercase(String[] input) {
+        for (int i = 0; i < input.length; i++) {
+            input[i] = input[i].toUpperCase();
+        }
+    }
+
+    public void incrementArray(int[] input) {
+        for (int i = 0; i < input.length; i++) {
+            input[i] = input[i] + 1;
+        }
+    }
+}
