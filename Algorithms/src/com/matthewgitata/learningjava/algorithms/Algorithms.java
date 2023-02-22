8b3fa79bd8705090a06ab8c915d6807d291a4977
Matthew Gitata <matthewgitata@gmail.com>
2023-02-22 22:14:00 +0300

    String: Input Normalization.

--- Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
+++ Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
@@ -1,18 +1,15 @@
 package com.matthewgitata.learningjava.algorithms;
 
+import java.util.Locale;
+
 public class Algorithms {
     public static void main(String[] args) {
-        System.out.println(isPasswordComplex("Hell0"));
-        System.out.println(isPasswordComplex("Hello"));
-        System.out.println(isPasswordComplex("hellO"));
-        System.out.println(isPasswordComplex("HeLLo"));
-        System.out.println(isPasswordComplex("hello"));
-        System.out.println(isPasswordComplex(" "));
+        System.out.println(normalizeString("     Hello There, BUDDY "));
     }
 
-    public static boolean isPasswordComplex(String s) {
-        return s.chars().anyMatch(Character::isDigit) &&
-                s.chars().anyMatch(Character::isUpperCase) &&
-                s.chars().anyMatch(Character::isLowerCase);
+    public static String normalizeString(String s) {
+        return s.toLowerCase(Locale.ROOT).
+                trim()
+                .replace(",", "");
     }
 }
