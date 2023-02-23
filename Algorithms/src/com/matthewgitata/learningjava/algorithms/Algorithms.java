8b634beb78ba210e444fa4d5016b3e5710c79ca2
Matthew Gitata <matthewgitata@gmail.com>
2023-02-23 07:36:00 +0300

    Arrays:Linear Search with Java Streams.

--- Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
+++ Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
@@ -1,19 +1,18 @@
 package com.matthewgitata.learningjava.algorithms;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.OptionalInt;
+
 public class Algorithms {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6};
         System.out.println(linearSearch(new int[]{}, 0));
-        System.out.println(linearSearch(arr, 1));
+        System.out.println(linearSearch(arr, 5));
         System.out.println(linearSearch(arr, 8));
     }
 
-    private static boolean linearSearch(int[] arr, int item) {
-        for (int current : arr) {
-            if (current == item) {
-                return true;
-            }
-        }
-        return false;
+    private static OptionalInt linearSearch(int[] arr, int item) {
+        return Arrays.stream(arr).filter(x -> x == item).findFirst();
     }
 }
