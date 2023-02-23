5ff2a54fdcbe81f7c63cf4b633f8dcdd32d6e76e
Matthew Gitata <matthewgitata@gmail.com>
2023-02-23 12:54:00 +0300

    LL:Delete back half of a LL.

--- Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
+++ Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
@@ -14,5 +14,7 @@ public class Algorithms {
         thirdNode.next = fourthNode;
 
         linkedList.displayContents();
+        linkedList.deleteBackHalf();
+        linkedList.displayContents();
     }
 }
