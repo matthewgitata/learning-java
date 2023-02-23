0b61a559691a841ef5188ca6635cb01664a030e1
Matthew Gitata <matthewgitata@gmail.com>
2023-02-23 10:18:00 +0300

    Arrays:Solution rotate an array.

--- Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
+++ Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
@@ -5,20 +5,33 @@ import java.util.Arrays;
 public class Algorithms {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-        rotateInPlace(arr);
+        rotateArrayLeft(arr);
         Arrays.stream(arr)
                 .forEach(System.out::print);
+        System.out.println();
+        rotateArrayRight(arr);
+        Arrays.stream(arr)
+                .forEach(System.out::print);
+        System.out.println();
     }
 
-    private static void rotateInPlace(int[] arr) {
+    private static void rotateArrayLeft(int[] arr) {
         int temp = arr[0];
-        arr[0] = arr[arr.length - 1];
-        arr[arr.length - 1] = temp;
-        for (int i = 1; i < arr.length - 2; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int shiftedIndex = i + 1;
-            int temp2 = arr[i];
             arr[i] = arr[shiftedIndex];
-            arr[shiftedIndex] = temp2;
         }
+        arr[arr.length - 1] = temp;
+
+    }
+
+    private static void rotateArrayRight(int[] arr) {
+        int temp = arr[arr.length - 1];
+        for (int i = arr.length -1; i > 0; i--) {
+            int shiftedIndex = i - 1;
+            arr[i] = arr[shiftedIndex];
+        }
+        arr[0] = temp;
+
     }
 }
