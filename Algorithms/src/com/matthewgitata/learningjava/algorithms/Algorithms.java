1c451b73e9d4e611a81c5795d10a82cc9a7128c5
Matthew Gitata <matthewgitata@gmail.com>
2023-02-23 18:12:00 +0300

    Stack:Matching parenthesis.

--- Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
+++ Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
@@ -4,38 +4,33 @@ import java.util.Stack;
 
 public class Algorithms {
     public static void main(String[] args) {
-        int[] arr = {16, 7, 2, 15};
-        printNextGreaterElement(arr);
+        System.out.println(hasMatchingParentheses("((algorithm()))"));
+        System.out.println(hasMatchingParentheses("()(algorithm())"));
+        System.out.println(hasMatchingParentheses("((algorithm))"));
+        System.out.println(hasMatchingParentheses("(algorithm)"));
+        System.out.println();
+        System.out.println(hasMatchingParentheses("(algorithm("));
+        System.out.println(hasMatchingParentheses(")algorithm)"));
+        System.out.println(hasMatchingParentheses(")algorithm("));
+        System.out.println(hasMatchingParentheses("algorithm(("));
+        System.out.println(hasMatchingParentheses("(algorithm(("));
+        System.out.println(hasMatchingParentheses("((algorithm)"));
     }
 
-    public static void printNextGreaterElement(int[] arr) {
-        if (arr.length == 0) {
-            System.out.println();
-            return;
-        }
-        Stack<Integer> stack = new Stack<>();
-        stack.push(arr[0]);
-
-        for (int i = 1; i < arr.length; i++) {
-            int next = arr[i];
-            if (!stack.isEmpty()) {
-                int popped = stack.pop();
-                while (popped < next) {
-                    System.out.println(popped + " --> " + next);
-                    if (stack.isEmpty()) {
-                        break;
-                    }
-                    popped = stack.pop();
-                }
-
-                if (popped > next) {
-                    stack.push(popped);
+    private static boolean hasMatchingParentheses(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (current == '(') {
+                stack.push(current);
+            } else if (current == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    return false;
                 }
             }
-            stack.push(next);
         }
-        while (!stack.isEmpty()) {
-            System.out.println(stack.pop() + " --> " + -1);
-        }
+        return stack.isEmpty();
     }
 }
