4722c99246e8e3ad965d4c4678aadd2df23f2a5d
Matthew Gitata <matthewgitata@gmail.com>
2023-02-23 16:05:00 +0300

    Queue: Generate Binary Numbers.

--- Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
+++ Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
@@ -1,24 +1,28 @@
 package com.matthewgitata.learningjava.algorithms;
 
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Algorithms {
     public static void main(String[] args) {
-        Queue<Integer> queue = new PriorityQueue<>();
-        queue.add(2);
-        queue.add(3);
-        queue.add(19);
-        queue.add(20);
+        printBinary(2);
+        printBinary(6);
+    }
 
-        int removedItem = queue.remove();
-        System.out.println(removedItem);
-        System.out.println(queue.remove());
-        System.out.println(queue.peek());
-
-        while (!queue.isEmpty()) {
-            System.out.println(queue.remove());
+    public static void printBinary(int n) {
+        if (n <= 0) {
+            System.out.println();
+            return;
         }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        for (int i = 0; i < n; i++) {
+            int current = queue.remove();
+            System.out.println(current);
+
+            queue.add(current * 10);
+            queue.add(current * 10 + 1);
+        }
+        System.out.println();
     }
 }
