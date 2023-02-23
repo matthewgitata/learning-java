b55e7a4edb2a4707eb1e01a1682adf7f6910658a
Matthew Gitata <matthewgitata@gmail.com>
2023-02-23 09:33:33 +0300

    Arrays: Reverse an Array.

--- Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
+++ Algorithms/src/com/matthewgitata/learningjava/algorithms/Algorithms.java
@@ -1,44 +1,31 @@
 package com.matthewgitata.learningjava.algorithms;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.function.IntPredicate;
-import java.util.stream.Stream;
 
 public class Algorithms {
     public static void main(String[] args) {
-        int[] arr1 = {-9, 3, 2, -8, 12, -16};
-        int[] arr2 = {0, -3, -8, -35, 40, 20, 7};
-        Arrays.stream(findEvenNums(arr1, arr2))
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        Arrays.stream(reverse(arr)).
+                forEach(System.out::println);
+        System.out.println();
+        reverseInPlace(arr);
+        Arrays.stream((arr))
                 .forEach(System.out::println);
-
-        Arrays.stream(findEvenNum2(arr1, arr2))
-                .forEach(System.out::print);
     }
 
-    private static int[] findEvenNums(int[] arr1, int[] arr2) {
-        ArrayList<Integer> evens = new ArrayList<>();
-        for (int i : arr1) {
-            if (i % 2 == 0) {
-                evens.add(i);
-            }
-        }
-        for (int i : arr2) {
-            if (i % 2 == 0) {
-                evens.add(i);
-            }
-        }
-        int[] result = new int[evens.size()];
-        for (int i = 0; i < evens.size(); i++) {
-            result[i] = evens.get(i);
+    private static int[] reverse(int[] arr) {
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = arr[result.length - 1 - i];
         }
         return result;
     }
 
-    public static int[] findEvenNum2(int[] arr1, int[] arr2) {
-        IntPredicate isEvenPred = num -> num % 2 == 0;
-        return Stream.of(arr1, arr2).flatMapToInt(Arrays::stream)
-                .filter(isEvenPred)
-                .toArray();
+    public static void reverseInPlace(int[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
     }
 }
